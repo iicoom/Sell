f27a3faa4ac47e084604d3ee0390cf77dd4e1382
Leo <asdfpeng@qq.com>
2018-07-16 10:44:34 +0800

    ProductService and Test

---
+++ src/main/java/com/icoom/sell/service/impl/ProductServiceImpl.java
@@ -0,0 +1,40 @@
+package com.icoom.sell.service.impl;
+
+import com.icoom.sell.dataobject.ProductInfo;
+import com.icoom.sell.enums.ProductStatusEnum;
+import com.icoom.sell.repository.ProductInfoRepository;
+import com.icoom.sell.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductInfoRepository repository;
+
+    @Override
+    public ProductInfo findOne(String productId) {
+        return repository.findById(productId).get();
+    }
+
+    @Override
+    public List<ProductInfo> findUpAll() {
+        return repository.findByProductStatus(ProductStatusEnum.UP.hashCode());
+    }
+
+    @Override
+    public Page<ProductInfo> findAll(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
+    @Override
+    public ProductInfo save(ProductInfo productInfo) {
+        return repository.save(productInfo);
+    }
+}
+
