5482f6216350ac4d0ba70e45672d1093a2180620
Leo <asdfpeng@qq.com>
2018-09-16 21:38:39 +0800

    微信授权开发流程

--- src/main/java/com/icoom/sell/controller/WechatController.java
+++ src/main/java/com/icoom/sell/controller/WechatController.java
@@ -6,14 +6,12 @@ import lombok.extern.slf4j.Slf4j;
 import me.chanjar.weixin.common.api.WxConsts;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpService;
-import me.chanjar.weixin.mp.api.impl.WxMpServiceImpl;
 import me.chanjar.weixin.mp.bean.result.WxMpOAuth2AccessToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URLEncoder;
 
@@ -34,7 +32,8 @@ public class WechatController {
 
         // 1. 配置
         // 2. 调用方法
-        String url = "http://sell.natapp4.cc/sell/wechat/userInfo";
+        // String url = "http://sell.natapp4.cc/sell/wechat/userInfo";
+        String url = "http://709ea02d.ngrok.io/sell/wechat/userInfo";
         String redirectUrl = wxMpService.oauth2buildAuthorizationUrl(url, WxConsts.OAuth2Scope.SNSAPI_USERINFO, URLEncoder.encode(returnUrl));
         log.info("【微信网页授权】获取code，result={}", redirectUrl);
 
